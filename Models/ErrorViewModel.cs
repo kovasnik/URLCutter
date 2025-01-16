c5eb2a4ce81afa0a8164bd042f930c3e04f91ad3
kovasnik <egorbf2003@gmail.com>
2025-01-16 13:25:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace URLCutter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
